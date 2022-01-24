995f065cb9e3c29bcd59762f72c412801c9bebe5
yoshiki-ota <football_18.y_o@icloud.com>
2022-01-24 21:38:42 +0900

    A8second commit A9first commit

--- A1/A8.py
+++ A1/A8.py
@@ -2,6 +2,17 @@ users_info = [["Bob", 79],
               ["Tom", 59],
               ["Ken", 61]]
 
+for info in users_info:
+    print(f"Name: {info[0]}, Age: {info[1]}")
+
+"""
+#test
+
+users_info = [["Bob", 79],
+              ["Tom", 59],
+              ["Ken", 61]]
+
 print(f"Name: {users_info[0][0]}, Age: {users_info[0][1]}")
 print(f"Name: {users_info[1][0]}, Age: {users_info[1][1]}")
 print(f"Name: {users_info[2][0]}, Age: {users_info[2][1]}")
+"""
