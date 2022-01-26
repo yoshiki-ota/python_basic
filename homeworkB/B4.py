82bfba5448845483e8fba4e373831c85eaf6df27
yoshiki-ota <football_18.y_o@icloud.com>
2022-01-26 21:42:16 +0900

    first commit
    全然わかりません

---
+++ homeworkB/B4.py
@@ -0,0 +1,31 @@
+def main():
+    # 3都府県のいくつかの駅名とある日の最高気温(単位: ℃)のデータを辞書として持っています
+    weather_information = [
+        {'prefecture': '東京都', 'station': '渋谷', 'temperature': 6.5},
+        {'prefecture': '東京都', 'station': '池袋', 'temperature': 7.0},
+        {'prefecture': '東京都', 'station': '新橋', 'temperature': 7.5},
+
+        {'prefecture': '大阪府', 'station': '梅田', 'temperature': 8.2},
+        {'prefecture': '大阪府', 'station': '大阪', 'temperature': 9.3},
+        {'prefecture': '大阪府', 'station': '堺', 'temperature': 9.5},
+
+        {'prefecture': '福岡県', 'station': '博多', 'temperature': 13.0},
+        {'prefecture': '福岡県', 'station': '太宰府', 'temperature': 15.0},
+    ]
+
+    # Q1. 全国の平均気温を計算してください(9.5となればOK)
+    i.get("temperature")
+    for i in weather_information
+        print(i)
+    # x = [i.get('temperature') for i in weather_information]
+    # print(int(x))
+    # print(f'{{x}/ int(len(weather_information))}')
+    # for count in range(1, (len(weather_information))+1):
+    # print(weather_information[count]['temperature'])
+    # Q2. 大阪府のすべての駅名をカンマ区切りで出力してください( '梅田,大阪,堺' となればOK)
+
+    # Q3. 福岡県の平均気温を計算してください(14.0となればOK)
+
+
+if __name__ == '__main__':
+    main()
