5fe8af79c43f0339ec17c539e44500c98116bf7c
yoshiki-ota <football_18.y_o@icloud.com>
2022-01-29 08:44:26 +0900

    皆さんの回答追加

--- homeworkA/A4.py
+++ homeworkA/A4.py
@@ -1,3 +1,6 @@
 member = ["Bob", "Tom", "Ken"]
-print(member[0])
-print(member[1])
+# print(member[0]) #自分
+# print(member[1])   #自分
+# print(member[:2])  #他回答
+print('\n'.join(member for member in member if member == 'Bob' or member == 'Tom'))  # 他回答
+print(member[0], member[1])
