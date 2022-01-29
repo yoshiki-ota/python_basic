cd3d4a6bce12b32382e518590bcb58311311f056
yoshiki-ota <football_18.y_o@icloud.com>
2022-01-29 10:07:52 +0900

    先生の回答追加

--- homeworkB/B5.py
+++ homeworkB/B5.py
@@ -4,6 +4,12 @@
 最大値: 21
 最小値: 1
 平均値: 6
+
+入力は「スペース区切り」
+    スペースは分解して配列にする　= 一つ一つに分ける
+最大値を求める
+    すべての値に対し大きい　一つ一つの数字に対し「総当たり」
+
 """
 
 date = [int(i) for i in input('データを入力してください(スペース区切り) > ').split()]
@@ -19,8 +25,8 @@ def date_sum1():
 
 print(f"合計値: {date_sum1()}")
 
-
 # 最大値: 21
+""" #自分で作成
 def date_max():
     count = 1
     dummy = 0
@@ -41,6 +47,22 @@ print(f"最大値: {date_max()}")
 
 # def date_max():
 # date_max2 = 0
+"""
+# 先生作成
+numbers_str = "1 1 2 3 5 8 13 21"
+numbers = numbers_str.split(" ")
+
+max = 0  # 0で初期化して関数を作成
+# もしくは↓　　最大や最小を求めるときはリストの１番最初を関数に定義付けしちゃう
+# max = int(numbers[0])
+
+for numbers in numbers:
+    # maxと比較して大きかったら↓
+    if int(numbers) > max:
+        # 入れ替えてしまう↓
+        max = int(numbers)
+print(max)
+
 
 # 最小値: 1
 def date_min():
